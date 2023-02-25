fc4dfd6f61cf2710c3b171bbdffbf6ae5143e12c
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-25 13:16:14 +0500

    views

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from flask import Flask
-from fruitDB import FruitDB
+from db import GroceryDB
+
+
 app = Flask(__name__)
-db = FruitDB()
-@app.route('/')
-def home():
-    fruits = db.all()
-    html ='<html>'
-    html += '<head><title>Fruit</title></head>'
-    html += '<body>'
-    html += '<h1>Fruit</h1>'
-    html += '<ul>'
-    for fruit in fruits:
-        html += f'<li>{fruit["fruit"]}  costs {fruit["price"]}</li>'
-    html += '</ul>'
-    html += '</body>'
-    html += '</html>'
-    return html
+db = GroceryDB()
+
+
+# view all grocery
+@app.route('/grocery')
+def all_grocery():
+    """Get all grocery"""
+    pass
+
+
+# view all grocery by type
+@app.route('/grocery/<type>')
+def all_grocery_by_type(type):
+    """Get all grocery by type"""
+    pass
+
+
+# view add grocery
+@app.route('/grocery/add', methods=['POST'])
+def add_grocery():
+    """Add a grocery"""
+    pass
 
 
 if __name__ == '__main__':
