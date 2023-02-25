d42f939a2563168b5a7011e85c986405e8f3a80c
ZARIFMAC <uzbzarif@gmail.com>
2023-02-25 10:45:25 +0500

    list of fruits

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
-
+from fruitDB import FruitDB
 app = Flask(__name__)
-
+db = FruitDB()
 @app.route('/')
 def home():
-    html = open('index.html', 'r').read()
+    fruits = db.all()
+    html ='<html>'
+    html += '<head><title>Fruit</title></head>'
+    html += '<body>'
+    html += '<h1>Fruit</h1>'
+    html += '<ul>'
+    for fruit in fruits:
+        html += f'<li>{fruit["fruit"]}  costs {fruit["price"]}</li>'
+    html += '</ul>'
+    html += '</body>'
+    html += '</html>'
     return html
 
 
